a4dca9c90084642e463100b62aa70df80c94b193
fmoro1129 <fmoro1129@gmail.com>
2024-09-13 16:50:17 -0300

    Update ejercicionum18.py

--- ejercicio/ejercicionum18.py
+++ ejercicio/ejercicionum18.py
@@ -1,4 +1,4 @@
-espacio=str(input("ingrese la cantidad de kilometros recorridos: "))
-tiempo=str(input("ingrese el tiempo empleado que tardo en recorrer esos kilometros: "))
+espacio=float(input("ingrese la cantidad de kilometros recorridos: "))
+tiempo=float(input("ingrese el tiempo empleado que tardo en recorrer esos kilometros: "))
 velocidad = espacio/tiempo 
 print (f"la velocidad a la que fue es: {velocidad}")
