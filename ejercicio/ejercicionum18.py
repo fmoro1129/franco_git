7fd1d2fdcbdccb2540a8f1507be33ef4ca86e3ae
fmoro1129 <fmoro1129@gmail.com>
2024-09-14 16:47:43 -0300

    Delete ejercicio directory

--- ejercicio/ejercicionum18.py
+++
@@ -1,4 +0,0 @@
-espacio=float(input("ingrese la cantidad de kilometros recorridos: "))
-tiempo=float(input("ingrese el tiempo empleado que tardo en recorrer esos kilometros: "))
-velocidad = espacio/tiempo 
-print (f"la velocidad a la que fue es: {velocidad}")
